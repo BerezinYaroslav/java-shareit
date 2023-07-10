1360eb75bf8586c8af439f252d4a9aff92daff71
Yaroslav Berezin <yb2003work@gmail.com>
2023-07-10 19:52:51 +0300

    feat: пробная

--- gateway/src/main/java/shareitgateway/ShareitGatewayApplication.java
+++ gateway/src/main/java/shareitgateway/ShareitGatewayApplication.java
@@ -1,12 +1,11 @@
-package ru.practicum.shareit;
+package shareitgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer {
+public class ShareitGatewayApplication {
 	public static void main(String[] args) {
-		System.setProperty("server.port", "9090");
-		SpringApplication.run(ShareItServer.class, args);
+		SpringApplication.run(ShareitGatewayApplication.class, args);
 	}
 }
